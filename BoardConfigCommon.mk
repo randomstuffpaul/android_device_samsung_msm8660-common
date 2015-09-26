7d38b5f83f2963c7c3ddcb72e350ad18d7de4977
bryan2894 <bryan2894@aospal.com>
2015-09-26 16:05:52 -0400

    msm8660: add TWRP support
    Change-Id: I6f036b6a915686d44c66d9a1ff4b605bf26e506a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,6 +93,7 @@ COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
 TARGET_USES_QCOM_BSP := true
 
 # Recovery
+TARGET_RECOVERY_DEVICE_DIRS := device/samsung/msm8660-common
 TARGET_RECOVERY_FSTAB := device/samsung/msm8660-common/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_F2FS := true
 
@@ -132,13 +133,38 @@ BOARD_SEPOLICY_UNION += \
     rmt_storage.te \
     secril-daemon.te \
     surfaceflinger.te \
-    sysinit.te \
     system_app.te \
     system_server.te \
     thermal-engine.te \
     ueventd.te \
     vold.te \
     wpa.te
+
+# TWRP
+DEVICE_RESOLUTION := 480x800
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXCLUDE_SUPERSU := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_CPU_TEMP := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p28"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "noauto_da_alloc"
+TW_CRYPTO_FS_FLAGS := "0x00000006"
+TW_CRYPTO_KEY_LOC := "/efs/metadata"
+TW_MAX_BRIGHTNESS := 255
+
 
 # Wifi related defines
 BOARD_HAVE_SAMSUNG_WIFI := true
